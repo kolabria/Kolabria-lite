e27b06cf95559d16cb630bbe1225126933f58b3d
= <=>
2012-05-15 22:27:00 -0400

    removed extra apps from WikiWall-app --not relevent

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.views.generic import TemplateView
-from django.conf.urls.defaults import url, patterns
-import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.home),
-)
