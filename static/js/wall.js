f321460ee059d32a78ffe26858d7fe2cbe97ad53
= <=>
2012-06-04 12:20:52 -0400

    added worker.js for wall.js to pull from on local /static/js/worker.js dir

--- static/js/wall.js
+++ static/js/wall.js
@@ -23,7 +23,7 @@ now.ready(function(){
   var color = 'black';
   var width = 2;
 
-  var worker = new Worker('/javascripts/worker.js');
+  var worker = new Worker('/static/js/worker.js');
   worker.addEventListener('message', function(e){
     pen.path.add(e.data);
     now.shareUpdateDraw(e.data,paper.project.activeLayer.index);
